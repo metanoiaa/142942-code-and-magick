15dc3c81dc30a7ed2ff8179833cdf7d9eb37c9b0
Nadi Ermakova <nadi@staal.io>
2017-02-14 12:52:57 +0100

    Обернула код в функции

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,18 +1,21 @@
 'use strict';
 
-// Изменение цвета мантии/глаз персонажа
-window.colorizeElement = function (element, colors, property) {
-  element.addEventListener('click', function () {
-    var currentColor = element.style[property];
-    var tmp = window.utils.getRandomElementExcept(colors, currentColor);
-    element.style[property] = tmp;
-  });
-
-  element.addEventListener('keydown', function (event) {
-    if (window.utils.isActivateEvent(event)) {
+(function () {
+  // Изменение цвета мантии/глаз персонажа
+  window.colorizeElement = function (element, colors, property) {
+    element.addEventListener('click', function () {
       var currentColor = element.style[property];
       var tmp = window.utils.getRandomElementExcept(colors, currentColor);
       element.style[property] = tmp;
-    }
-  });
-};
+    });
+
+    element.addEventListener('keydown', function (event) {
+      if (window.utils.isActivateEvent(event)) {
+        var currentColor = element.style[property];
+        var tmp = window.utils.getRandomElementExcept(colors, currentColor);
+        element.style[property] = tmp;
+      }
+    });
+  };
+})();
+
